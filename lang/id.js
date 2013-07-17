59878229bb4df67549039f06bb8ec7c45ec5726b
Wiktor Walc <w.walc@cksource.com>
2013-07-17 17:38:41 +0200

    Updated translations, added Indonesian language.

--- lang/id.js
+++ lang/id.js
@@ -2,7 +2,7 @@
 Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang( 'bidi', 'sl', {
-	ltr: 'Smer besedila od leve proti desni',
-	rtl: 'Smer besedila od desne proti levi'
+CKEDITOR.plugins.setLang( 'bidi', 'id', {
+	ltr: 'Arah penulisan dari kiri ke kanan.',
+	rtl: 'Arah penulisan dari kanan ke kiri.'
 });
